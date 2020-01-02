4cc8d56fa68b3afb65c2ea97ba2a7490ceed2971
Ben Alkov <ben.alkov@gmail.com>
2020-01-01 21:50:17 -0500

    LuaFormatter; luac -p. Indent was inconsistent - I settled on 4 spaces

--- lib/midigrid.lua
+++ lib/midigrid.lua
@@ -1,36 +1,29 @@
--- [[--
+--[[ cheapskate lib for getting midi grid devices to behave like monome grid devices
+     local midigrid ={}
+     two things are run before returning, 'setup_connect_handling' and 'update_devices'
+     setup_connect_handling copies over 'og' midi add and remove callbacks, and gives its own
+     add and remove handlers, which means the call backs for
+     add and remove handlers:
+     update devices:
+     find_midi_devices: iterates through 'midi.devices' to see if the name matches, then
+     returns id, this system manages its own ids, which is why you have to initialize it and
+     why
+     first, you connect to it, 'midigrid.connect', which returns a midigrid object and does
+     'set_midi_handler'
+]]
 
--- cheapskate lib for getting midi grid devices to behave like monome grid devices
---   --]]
--- local midigrid ={}
---two things are run before returning, 'setup_connect_handling' and 'update_devices'
---setup_connect_handling copies over 'og' midi add and remove callbacks, and gives its own add and remove handlers, which means the call backs for 
---add and remove handlers:
---update devices:
---find_midi_devices: iterates through 'midi.devices' to see if the name matches, then returns id, this system manages its own ids, which is why you have to initialize it and why
---first, you connect to it, 'midigrid.connect', which returns a midigrid object and does 'set_midi_handler'
---
------------------------------
---loading up config file here
------------------------------
+-- loading up config file here
 local config = include('midigrid/config/apcmini_config')
 -- local config = include('midigrid/config/launchpad_config')
 -- local config = include('midigrid/config/untz_config')
------------------------------
------------------------------
------------------------------
-
 local gridnotes = config.grid
 local brightness_handler = config.brightness_handler
 local device_name = config.device_name
------------------------------
---adding midi device call backs
---------------------------------
-local midigrid= {
-  midi_id = nil
-}
 
+-- adding midi device call backs---
+local midigrid = {midi_id = nil}
 local og_dev_add, og_dev_remove
+
 
 function midigrid.find_midi_device_id()
     local found_id = nil
@@ -43,33 +36,35 @@ function midigrid.find_midi_device_id()
     return found_id
 end
 
+
 function midigrid.connect(dummy_id)
     midigrid.set_midi_handler()
     return midigrid
 end
+
 
 -- function midigrid.set_key_handler(key_handler)
 --     midigrid.set_midi_handler()
 --     midigrid.key = key_handler
 -- end
 
+
 function midigrid.setup_connect_handling()
     og_dev_add = midi.add
     og_dev_remove = midi.remove
-
     midi.add = midigrid.handle_dev_add
     midi.remove = midigrid.handle_dev_remove
 end
+
 
 function midigrid.name_matches(name)
     return (name == device_name)
 end
 
+
 function midigrid.handle_dev_add(id, name, dev)
     og_dev_add(id, name, dev)
-
     midigrid.update_devices()
-
     if (midigrid.name_matches(name)) and (id ~= midigrid.midi_id) then
         midigrid.midi_id = id
         midigrid.device = dev
@@ -77,124 +72,129 @@ function midigrid.handle_dev_add(id, name, dev)
     end
 end
 
+
 function midigrid.handle_dev_remove(id)
     og_dev_remove(id)
     midigrid.update_devices()
 end
 
---this already expects it to have Midi_id
-function midigrid.set_midi_handler()
-    if midigrid.midi_id == nil then
-        return
-    end
 
+-- this already expects it to have Midi_id
+function midigrid.set_midi_handler()
+    if midigrid.midi_id == nil then return end
     if midi.devices[midigrid.midi_id] ~= nil then
         midi.devices[midigrid.midi_id].event = midigrid.handle_key_midi
-        --need this for checking .device
-        midigrid.device=midi.devices[midigrid.midi_id]
+
+        -- need this for checking .device
+        midigrid.device = midi.devices[midigrid.midi_id]
     else
         midigrid.midi_id = nil
     end
 end
 
+
 function midigrid.cleanup()
-  midigrid.key = nil
+    midigrid.key = nil
 end
+
 
 function midigrid.update_devices()
-  midi.update_devices()
+    midi.update_devices()
+    local new_id = midigrid.find_midi_device_id()
 
-  local new_id = midigrid.find_midi_device_id()
-
-  -- Only set id/handler when helpful
-  if (midigrid.midi_id ~= new_id) and (new_id ~= nil) then
-    midigrid.midi_id = new_id
-    return midigrid.set_midi_handler()
-  end
-
-  return (midigrid.midi_id ~= nil)
+    -- Only set id/handler when helpful
+    if (midigrid.midi_id ~= new_id) and (new_id ~= nil) then
+        midigrid.midi_id = new_id
+        return midigrid.set_midi_handler()
+    end
+    return (midigrid.midi_id ~= nil)
 end
 
 
-
---here, using the grid from the config file, we generate the table to help us go the other way around
---so, if you press a midi note and you wanna know what it is, this will have an index with our coordinates
-local note2coords={}
-
-for i,v in ipairs(gridnotes) do
-  for j,k in ipairs(v) do
-    note2coords[k]={j,i}
-  end
+-- here, using the grid from the config file, we generate the table to help us go the other
+-- way around so, if you press a midi note and you wanna know what it is, this will have an
+-- index with our coordinates
+local note2coords = {}
+for i, v in ipairs(gridnotes) do
+    for j, k in ipairs(v) do
+        note2coords[k] = {j, i}
+    end
 end
-
-midigrid.ledbuf={}
-
+midigrid.ledbuf = {}
 midigrid.rows = #gridnotes[1]
 midigrid.cols = #gridnotes
 
+
 function midigrid.handle_key_midi(event)
-  --block cc messages, so they can be mapped
-  if(event[1]==0x90 or event[1]==0x80) then
-    local note = event[2]
-    local coords = note2coords[note]
-    local x, y
-    if coords then
-      x, y = coords[1],coords[2]
-      local s = event[1] ==0x90 and 1 or 0
-      if midigrid.key ~= nil then
-        midigrid.key(x, y, s)
-      end
-    else
-      print("missing coords!")
+    -- block cc messages, so they can be mapped
+    if (event[1] == 0x90 or event[1] == 0x80) then
+        local note = event[2]
+        local coords = note2coords[note]
+        local x, y
+        if coords then
+            x, y = coords[1], coords[2]
+            local s = event[1] == 0x90 and 1 or 0
+            if midigrid.key ~= nil then
+                midigrid.key(x, y, s)
+            end
+        else
+            print("missing coords!")
+        end
     end
-  end
 end
 
 
 function midigrid:led(x, y, z)
-  if self.device then
-    chan = 1
-    --flag reversed here because thats actually what it is in lua table!!!, see above. this is clearer either way I think
-    note = ((x<9 and x>0) and (y<9 and y>0)) and gridnotes[y][x] or null
-    vel = brightness_handler(z)
-    if note then
-      table.insert(self.ledbuf,0x90)
-      table.insert(self.ledbuf,note)
-      table.insert(self.ledbuf,vel)
-    else
-      --debugger, probably want to comment this out if you are being messyy
-      print("no note found! coordinates....  x:"..x.."  y:"..y.."  z:"..z)
+    if self.device then
+        chan = 1
+
+        -- flag reversed here because thats actually what it is in lua table!!!, see above.
+        -- this is clearer either way I think
+        note = ((x < 9 and x > 0) and (y < 9 and y > 0)) and gridnotes[y][x] or nil
+        vel = brightness_handler(z)
+        if note then
+            table.insert(self.ledbuf, 0x90)
+            table.insert(self.ledbuf, note)
+            table.insert(self.ledbuf, vel)
+        else
+
+            -- debugger, probably want to comment this out if you are being messyy
+            print("no note found! coordinates....  x:" .. x .. "  y:" .. y ..
+                      "  z:" .. z)
+        end
     end
-  end
 end
 
 
---sending our buff
+-- sending our buff
 function midigrid:refresh()
-  if self.device then
-    -- self:send(self.ledbuf)
-    midi.devices[midigrid.midi_id]:send(self.ledbuf)
-    self.ledbuf={}
-  end
+    if self.device then
+        -- self:send(self.ledbuf)
+        midi.devices[midigrid.midi_id]:send(self.ledbuf)
+        self.ledbuf = {}
+    end
 end
+
 
 function midigrid:all(vel)
-  if self.device then
-    self.ledbuf={}
-    for x=1, #gridnotes do
-      for y=1, #gridnotes[x] do
-        chan = 1
-        note = gridnotes[x][y]
-        vel = brightness_handler(vel)
-        table.insert(self.ledbuf,0x90)
-        table.insert(self.ledbuf,note)
-        table.insert(self.ledbuf,vel)
-      end
-      -- it is unclear to me sometimes if a call to all in a regular grid requires a subsequent refresh, have this here in case
-      -- self:refresh()
+    if self.device then
+        self.ledbuf = {}
+        for x = 1, #gridnotes do
+            for y = 1, #gridnotes[x] do
+                chan = 1
+                note = gridnotes[x][y]
+                vel = brightness_handler(vel)
+                table.insert(self.ledbuf, 0x90)
+                table.insert(self.ledbuf, note)
+                table.insert(self.ledbuf, vel)
+            end
+            -- it is unclear to me sometimes if a call to all in a regular grid requires a
+            -- subsequent refresh, have this here in cqase
+            -- self:refresh()
+        end
     end
-  end
 end
+
 
 -- setting up connection and connection callbacks before returning
 midigrid.setup_connect_handling()
--- lib/midigrid_2pages.lua
+++ lib/midigrid_2pages.lua
@@ -1,49 +1,37 @@
--- [[--
---  cheapskate library for apc, 2 pages
---  contains within itself a full 128 grid table, that can be viewed and played with by changing the two buttons at the bottom of the apc
--- --]]
+--[[ cheapskate library for apc, 2 pages contains within itself a full 128 grid table, that
+     can be viewed and played with by changing the two buttons at the bottom of the apc
+]]
 
------------------------------
---loading up config file here
------------------------------
+--  loading up config file here
 local config = include('midigrid/config/apcmini_config')
 -- local config = include('midigrid/config/launchpad_config')
------------------------------
------------------------------
------------------------------
 
+-- start on "page" 1
+local apcpage = 1
 
-
-
-
---start on "page" 1
-local apcpage=1
-
---make the grid buf
-local gridbuf={}
-for i=1,16 do
-  gridbuf[i]={}
-  for j=1,8 do
-    gridbuf[i][j]=0
-  end
+-- make the grid buf
+local gridbuf = {}
+for i = 1, 16 do
+    gridbuf[i] = {}
+    for j = 1, 8 do
+        gridbuf[i][j] = 0
+    end
 end
 
 local gridnotes = config.grid
 local brightness_handler = config.brightness_handler
 local device_name = config.device_name
---set your left right page numbers here...
+
+-- set your left right page numbers here...
 -- local leftpage = config.auxrow[3]
 local leftpage = config.leftpage_button
 local rightpage = config.rightpage_button
 -- local rightpage = config.auxrow[4]
------------------------------
---adding midi device call backs
---------------------------------
-local midigrid= {
-  midi_id = nil
-}
 
+-- adding midi device call backs
+local midigrid = {midi_id = nil}
 local og_dev_add, og_dev_remove
+
 
 function midigrid.find_midi_device_id()
     local found_id = nil
@@ -56,35 +44,37 @@ function midigrid.find_midi_device_id()
     return found_id
 end
 
+
 function midigrid.connect(dummy_id)
     midigrid.set_midi_handler()
-    midi.devices[midigrid.midi_id]:send({144,leftpage,1})
-    midi.devices[midigrid.midi_id]:send({144,rightpage,0})
+    midi.devices[midigrid.midi_id]:send({144, leftpage, 1})
+    midi.devices[midigrid.midi_id]:send({144, rightpage, 0})
     return midigrid
 end
+
 
 -- function midigrid.set_key_handler(key_handler)
 --     midigrid.set_midi_handler()
 --     midigrid.key = key_handler
 -- end
 
+
 function midigrid.setup_connect_handling()
     og_dev_add = midi.add
     og_dev_remove = midi.remove
-
     midi.add = midigrid.handle_dev_add
     midi.remove = midigrid.handle_dev_remove
 end
+
 
 function midigrid.name_matches(name)
     return (name == device_name)
 end
 
+
 function midigrid.handle_dev_add(id, name, dev)
     og_dev_add(id, name, dev)
-
     midigrid.update_devices()
-
     if (midigrid.name_matches(name)) and (id ~= midigrid.midi_id) then
         midigrid.midi_id = id
         midigrid.device = dev
@@ -92,230 +82,232 @@ function midigrid.handle_dev_add(id, name, dev)
     end
 end
 
+
 function midigrid.handle_dev_remove(id)
     og_dev_remove(id)
     midigrid.update_devices()
 end
 
---this already expects it to have Midi_id
-function midigrid.set_midi_handler()
-    if midigrid.midi_id == nil then
-        return
-    end
 
+-- this already expects it to have Midi_id
+function midigrid.set_midi_handler()
+    if midigrid.midi_id == nil then return end
     if midi.devices[midigrid.midi_id] ~= nil then
         midi.devices[midigrid.midi_id].event = midigrid.handle_key_midi
-        --need this for checking .device
-        midigrid.device=midi.devices[midigrid.midi_id]
+
+        -- need this for checking .device
+        midigrid.device = midi.devices[midigrid.midi_id]
     else
         midigrid.midi_id = nil
     end
 end
 
-function midigrid.cleanup()
-  midigrid.key = nil
-end
+
+function midigrid.cleanup() midigrid.key = nil end
+
 
 function midigrid.update_devices()
-  midi.update_devices()
+    midi.update_devices()
+    local new_id = midigrid.find_midi_device_id()
 
-  local new_id = midigrid.find_midi_device_id()
-
-  -- Only set id/handler when helpful
-  if (midigrid.midi_id ~= new_id) and (new_id ~= nil) then
-    midigrid.midi_id = new_id
-    return midigrid.set_midi_handler()
-  end
-
-  return (midigrid.midi_id ~= nil)
+    -- Only set id/handler when helpful
+    if (midigrid.midi_id ~= new_id) and (new_id ~= nil) then
+        midigrid.midi_id = new_id
+        return midigrid.set_midi_handler()
+    end
+    return (midigrid.midi_id ~= nil)
 end
 
-aliascoords_event={}
-aliascoords_led={}
-function midigrid.make_alias(note,x,y)
-  aliascoords_event[note]={x,y}
-  aliascoords_led[x+(y*8)]=note
-end
-function midigrid.check_alias_event(note,noteon)
-  if aliascoords_event[note] then
-    local x = aliascoords_event[note][1]
-    local y = aliascoords_event[note][2]
-    local s = noteon ==0x90 and 1 or 0
-    midigrid.key(x,y,s)
-  end
-end
-function midigrid.check_alias_led(x,y,z)
-  if aliascoords_led[x+(y*8)] then
-      table.insert(midigrid.ledbuf,0x90)
-      table.insert(midigrid.ledbuf,aliascoords_led[x+(y*8)])
-      local vel = z ==0 and 0 or 1
-      table.insert(midigrid.ledbuf,vel)
-      -- print('led call',x,y,z)
-  end
+
+aliascoords_event = {}
+aliascoords_led = {}
+function midigrid.make_alias(note, x, y)
+    aliascoords_event[note] = {x, y}
+    aliascoords_led[x + (y * 8)] = note
 end
 
---getting the two pages set up
-apcnotecoords1={}
-apcnotecoords2={}
-for i,v in ipairs(gridnotes) do
-  for j,k in ipairs(v) do
-    apcnotecoords1[k]={j,i}
-  end
+
+function midigrid.check_alias_event(note, noteon)
+    if aliascoords_event[note] then
+        local x = aliascoords_event[note][1]
+        local y = aliascoords_event[note][2]
+        local s = noteon == 0x90 and 1 or 0
+        midigrid.key(x, y, s)
+    end
 end
 
-for i,v in ipairs(gridnotes) do
-  for j,k in ipairs(v) do
-    apcnotecoords2[k]={j+8,i}
-  end
+
+function midigrid.check_alias_led(x, y, z)
+    if aliascoords_led[x + (y * 8)] then
+        table.insert(midigrid.ledbuf, 0x90)
+        table.insert(midigrid.ledbuf, aliascoords_led[x + (y * 8)])
+        local vel = z == 0 and 0 or 1
+        table.insert(midigrid.ledbuf, vel)
+        -- print('led call',x,y,z)
+    end
 end
-apcnotecoords = {apcnotecoords1,apcnotecoords2}
 
 
-midigrid.ledbuf={}
-
+-- getting the two pages set up
+apcnotecoords1 = {}
+apcnotecoords2 = {}
+for i, v in ipairs(gridnotes) do
+    for j, k in ipairs(v) do
+        apcnotecoords1[k] = {j, i}
+    end
+end
+for i, v in ipairs(gridnotes) do
+    for j, k in ipairs(v) do
+        apcnotecoords2[k] = {j + 8, i}
+    end
+end
+apcnotecoords = {apcnotecoords1, apcnotecoords2}
+midigrid.ledbuf = {}
 midigrid.rows = #gridnotes[1]
 midigrid.cols = #gridnotes
 
 
-function midigrid:led(x, y, z) 
-  midigrid.check_alias_led(x,y,z)
-  if x < 17 and y<9 and x >0 and y >0 then
+function midigrid:led(x, y, z)
+    midigrid.check_alias_led(x, y, z)
+    if x < 17 and y < 9 and x > 0 and y > 0 then
+        vel = brightness_handler(z)
+        gridbuf[x][y] = z
 
-    vel = brightness_handler(z)
-    gridbuf[x][y]=z
-    --if we aint on the right page dont bother
-    if x>8 and apcpage==1 then
-      return
+        -- if we aint on the right page dont bother
+        if x > 8 and apcpage == 1 then
+            return
+        end
+        if x < 8 and apcpage == 2 then
+            return
+        end
+        if self.device then
+            chan = 1
+            if apcpage == 1 then
+                note = gridnotes[y][x]
+            elseif apcpage == 2 then
+                note = gridnotes[y][x - 8]
+            end
+            if note then
+                table.insert(self.ledbuf, 0x90)
+                table.insert(self.ledbuf, note)
+                table.insert(self.ledbuf, vel)
+                -- print('led call',x,y,z)
+            else
+
+                -- debugger
+                print(
+                    "no note found! coordinates....  x:" .. x .. "  y:" .. y .. "  z:" .. z)
+            end
+        end
     end
-    if x<8 and apcpage==2 then
-      return
-    end
-
-    if self.device then
-      chan = 1
-
-      if apcpage==1 then
-        note = gridnotes[y][x] 
-      elseif apcpage==2 then
-        note = gridnotes[y][x-8]
-      end
-
-      if note then
-        table.insert(self.ledbuf,0x90)
-        table.insert(self.ledbuf,note)
-        table.insert(self.ledbuf,vel)
-        -- print('led call',x,y,z)
-      else
-        --debugger
-        print("no note found! coordinates....  x:"..x.."  y:"..y.."  z:"..z)
-      end
-    end
-  end
 end
+
 
 -- sure there is more elegant way!
 function midigrid:changepage(page)
-  midigrid.ledbuf={}
-  -- self:all(4)
-  if page==1 then
-    for i=1,8 do
-      for j=1,8 do
-        midigrid:led(i,j,gridbuf[i][j])
-      end
+    midigrid.ledbuf = {}
+    -- self:all(4)
+    if page == 1 then
+        for i = 1, 8 do
+            for j = 1, 8 do
+                midigrid:led(i, j, gridbuf[i][j])
+            end
+        end
+    elseif page == 2 then
+        for i = 9, 16 do
+            for j = 1, 8 do
+                -- print('page2',gridbuf[i][j])
+                midigrid:led(i, j, gridbuf[i][j])
+            end
+        end
     end
-  elseif page==2 then
-    for i=9,16 do
-      for j=1,8 do
-        -- print('page2',gridbuf[i][j])
-        midigrid:led(i,j,gridbuf[i][j])
-      end
-    end
-  end
-  midigrid:refresh()
+    midigrid:refresh()
 end
-
 
 
 function midigrid.handle_key_midi(data)
-  note = data[2]
-  midigrid.check_alias_event(data[2],data[1])
-  --first, intercept page selectors
-  if note==leftpage or note==rightpage then
-    if note==leftpage and data[1]==0x90 and apcpage ~= 1 then
-      apcpage=1
-      -- midi.devices[midigrid.midi_id]:send({type="note_on",ch=1,note=rightpage,vel=0})
-      midi.devices[midigrid.midi_id]:send({144,rightpage,0})
-      midi.devices[midigrid.midi_id]:send({144,leftpage,1})
-      -- midi.devices[midigrid.midi_id]:send({type="note_on",ch=1,note=leftpage,vel=1})
-      midigrid:changepage(apcpage)
-    elseif note==rightpage and data[1]==0x90 and apcpage ~= 2  then
-      apcpage=2
-      -- midi.devices[midigrid.midi_id]:send({type="note_on",ch=1,note=leftpage,vel=0})
-      -- midi.devices[midigrid.midi_id]:send({type="note_on",ch=1,note=rightpage,vel=1})
-      midi.devices[midigrid.midi_id]:send({144,rightpage,1})
-      midi.devices[midigrid.midi_id]:send({144,leftpage,0})
-      midigrid:changepage(apcpage)
-    end
-  elseif note > -1 and note < 64 then
-    local coords = apcnotecoords[apcpage][note]
-    local x, y
-    if coords then
-      x, y = coords[1],coords[2] 
-      local s = data[1] ==0x90 and 1 or 0
-      midigrid.key(x,y,s)
+    note = data[2]
+    midigrid.check_alias_event(data[2], data[1])
+
+    -- first, intercept page selectors
+    if note == leftpage or note == rightpage then
+        if note == leftpage and data[1] == 0x90 and apcpage ~= 1 then
+            apcpage = 1
+            -- midi.devices[midigrid.midi_id]:send({type="note_on",ch=1,note=rightpage,vel=0})
+            midi.devices[midigrid.midi_id]:send({144, rightpage, 0})
+            midi.devices[midigrid.midi_id]:send({144, leftpage, 1})
+            -- midi.devices[midigrid.midi_id]:send({type="note_on",ch=1,note=leftpage,vel=1})
+            midigrid:changepage(apcpage)
+        elseif note == rightpage and data[1] == 0x90 and apcpage ~= 2 then
+            apcpage = 2
+            -- midi.devices[midigrid.midi_id]:send({type="note_on",ch=1,note=leftpage,vel=0})
+            -- midi.devices[midigrid.midi_id]:send({type="note_on",ch=1,note=rightpage,vel=1})
+            midi.devices[midigrid.midi_id]:send({144, rightpage, 1})
+            midi.devices[midigrid.midi_id]:send({144, leftpage, 0})
+            midigrid:changepage(apcpage)
+        end
+    elseif note > -1 and note < 64 then
+        local coords = apcnotecoords[apcpage][note]
+        local x, y
+        if coords then
+            x, y = coords[1], coords[2]
+            local s = data[1] == 0x90 and 1 or 0
+            midigrid.key(x, y, s)
+        else
+            local coords = apcnotecoords[apcpage][note]
+            local x, y
+            print("missing coords!", x, y, s)
+        end
     else
-      local coords = apcnotecoords[apcpage][note]
-      local x, y
-      print("missing coords!",x,y,s)
+        print("unmapped key")
     end
-  else
-    print("unmapped key")
-  end
 end
 
-function midigrid:refresh() 
-  if self.device then
-    -- self:send(self.ledbuf)
-    midi.devices[midigrid.midi_id]:send(self.ledbuf)
-    self.ledbuf={}
-  end
+
+function midigrid:refresh()
+    if self.device then
+        -- self:send(self.ledbuf)
+        midi.devices[midigrid.midi_id]:send(self.ledbuf)
+        self.ledbuf = {}
+    end
 end
+
 
 function midigrid:all(z)
-  vel = brightness_handler(z)
-  if self.device then
-    -- self.ledbuf={}
-    for x=1, 16 do
-      for y=1, 8 do
-        local data
-        local oldvel = gridbuf[x][y]
-        gridbuf[x][y]=z
-        if gridbuf[x][y]~=oldvel then
-          -- gridbuf[x][y]=vel
-          midigrid.check_alias_led(x,y,z)
-          if (apcpage==1 and x<9) then
-            note = gridnotes[y][x] 
-            table.insert(self.ledbuf,0x90)
-            table.insert(self.ledbuf,note)
-            table.insert(self.ledbuf,vel)
-          elseif (apcpage==2 and x>8) then
-            note = gridnotes[y][x-8]
-            table.insert(self.ledbuf,0x90)
-            table.insert(self.ledbuf,note)
-            table.insert(self.ledbuf,vel)
-          end
-          -- print('all call',x,y,vel)
+    vel = brightness_handler(z)
+    if self.device then
+        -- self.ledbuf={}
+        for x = 1, 16 do
+            for y = 1, 8 do
+                local data
+                local oldvel = gridbuf[x][y]
+                gridbuf[x][y] = z
+                if gridbuf[x][y] ~= oldvel then
+                    -- gridbuf[x][y]=vel
+                    midigrid.check_alias_led(x, y, z)
+                    if (apcpage == 1 and x < 9) then
+                        note = gridnotes[y][x]
+                        table.insert(self.ledbuf, 0x90)
+                        table.insert(self.ledbuf, note)
+                        table.insert(self.ledbuf, vel)
+                    elseif (apcpage == 2 and x > 8) then
+                        note = gridnotes[y][x - 8]
+                        table.insert(self.ledbuf, 0x90)
+                        table.insert(self.ledbuf, note)
+                        table.insert(self.ledbuf, vel)
+                    end
+                    -- print('all call',x,y,vel)
+                end
+            end
+            -- if this is needed
+            -- midi.devices[midigrid.midi_id]:send(self.ledbuf)
+            -- self.ledbuf={}
         end
-      end
-      -- if this is needed
-      -- midi.devices[midigrid.midi_id]:send(self.ledbuf)
-      -- self.ledbuf={}
     end
-  end
 end
---init on page 1
+
+-- init on page 1
 midigrid.setup_connect_handling()
 midigrid.update_devices()
-
 
 return midigrid
