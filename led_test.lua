b5e2926696410ec89e5c0ea237668f4b526faeca
Jeremy Arnold <497595+jaggednz@users.noreply.github.com>
2020-02-12 23:27:38 +1300

    A simple midi grid animation test.

---
+++ led_test.lua
@@ -0,0 +1,50 @@
+local grid = include "midigrid/lib/midigrid"
+local g = grid.connect()
+
+local grid_metro = metro.init()
+
+local grid_clock = 0
+
+local light_pattern = {}
+for i = 1,16 do
+  light_pattern[i] = i-1
+end
+
+grid_metro.event = function()
+    --print("beat")
+    local light_level = 0
+    for x = 1,8 do
+      -- Two rows showing full LED scale
+      for y = 1,2 do
+        light_level = x + (y*8)-8
+        g:led(x,y,light_pattern[light_level])
+      end
+      -- Rest of the rows scrolling 
+      for y = 3,8 do
+        light_level = ((x+(y-1)+(grid_clock)) % 16)+1
+        g:led(x,y,light_pattern[light_level])
+      end
+    end
+    
+    g:refresh()
+    
+    grid_clock = (grid_clock+1)
+end
+
+function init()
+  print('init')
+   grid_metro:start((0.06))
+   -- g.all(0)
+end
+  
+function redraw()
+
+end
+
+function key(n, z)
+
+end
+
+function enc(n, d)
+
+end
