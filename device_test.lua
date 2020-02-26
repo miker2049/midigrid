bf91ddeb0af87c523e022eb26c4910b954bad0db
Jeremy Arnold <497595+jaggednz@users.noreply.github.com>
2020-02-26 22:52:44 +1300

    Basic test drawing to multiple devices

---
+++ device_test.lua
@@ -0,0 +1,20 @@
+local device = include('midigrid/devices/launchpad')
+local device2 = include('midigrid/devices/launchpad')
+
+device.midi_id = 2
+device2.midi_id = 1
+
+device:_reset()
+device2:_reset()
+
+for i = 1,8 do
+  device:_update_led(i,4,i-1)
+  device2:_update_led(i,4,i-1)
+end
+for i = 1,8 do
+  device:_update_led(i,5,i+7)
+  device2:_update_led(i,5,i+7)
+end
+
+device:_update_led(1,1,15)
+device2:_update_led(2,1,15)
