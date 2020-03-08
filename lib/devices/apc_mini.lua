712b756e4733f676eac8276c7af8ed56ac536a48
Jeremy Arnold <497595+jaggednz@users.noreply.github.com>
2020-03-08 23:00:03 +1300

    fix apc device typo

--- lib/devices/apc_mini.lua
+++ lib/devices/apc_mini.lua
@@ -1,6 +1,6 @@
 local apcmini = include('midigrid/lib/devices/generic_device')
 
-apcmini.brightness_map = {0,1,1,1,1,1,1,1,3,3,3,3,5,5,5,5},
+apcmini.brightness_map = {0,1,1,1,1,1,1,1,3,3,3,3,5,5,5,5}
 
 apcmini.device_name = 'APC Mini'
 
