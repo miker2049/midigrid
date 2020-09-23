9e7ea549d8884d463d25f8564c9d238da14301d6
robmckinnon <rob@movingflow>
2020-09-23 23:02:21 +0100

    Add config for launchpad mini mk3

--- lib/supported_devices.lua
+++ lib/supported_devices.lua
@@ -2,6 +2,8 @@ local supported_devices = {
   midi_devices = {
     {  midi_base_name= 'launchpad',        device_type='launchpad'     },
     {  midi_base_name= 'launchpad mini',   device_type='launchpad'     },
+    {  midi_base_name= 'launchpad mini mk3 1', device_type='launchpad_minimk3' },
+    {  midi_base_name= 'launchpad mini mk3 2', device_type='launchpad_minimk3' },
     {  midi_base_name= 'apc mini',         device_type='apc_mini'      },
     {  midi_base_name= 'block 1',          device_type='livid_block'   },
 --[[ Known devices to be implemented, help required!
